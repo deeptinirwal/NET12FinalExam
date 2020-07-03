e2d59a8ce49a78ae3f23a90a4be50fcfb828bcbe
deeptinirwal <66492566+deeptinirwal@users.noreply.github.com>
2020-07-02 20:23:38 -0400

    first commit

---
+++ NET12Final/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace NET12Final
+{
+    class ProgramFinal
+    {
+        static void Main(string[] args)
+        {
+            Adult[] ad = new Adult[10];
+            Child[] cd = new Child[10];
+
+            for(int i=0;i<ad.Length;i++)
+            {
+                Console.WriteLine("Enter the First Name and Last Name");
+                ad[i].FirstName = Console.ReadLine();
+                ad[i].LastName = Console.ReadLine();
+            }
+            Console.WriteLine("First Names and Last Names are");
+            for (int i = 0; i < ad.Length; i++)
+            {
+                ad[i].PrintAName();
+            }
+
+            for (int i = 0; i < cd.Length; i++)
+            {
+                Console.WriteLine("Enter the First Name and Last Name");
+                cd[i].FirstName = Console.ReadLine();
+                 cd[i].LastName = Console.ReadLine();
+            }
+            Console.WriteLine("First Names and Last Names are");
+            for (int i = 0; i < ad.Length; i++)
+            {
+                cd[i].PrintCName();
+            }
+
+
+
+
+        }
+        public class Adult
+    {
+        public string FirstName;
+        public string LastName;
+        public void PrintAName()
+        {
+            Console.WriteLine("First Name is {0} Last Name is {1}", FirstName, LastName);
+        }
+    }
+
+    public class Child 
+    {
+        public string FirstName;
+        public string LastName;
+        public void PrintCName()
+        {
+        Console.WriteLine("First Name is {0} Last Name is {1}", FirstName, LastName);
+        }
+    }
+}
+}
